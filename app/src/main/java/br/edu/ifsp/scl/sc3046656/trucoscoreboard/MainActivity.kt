013132ad983bd363b989064c0913c5a466dbddb1
C4rloos0 <c.alexandreecorrea@gmail.com>
2026-08-18 13:14:42 -0300

    feat: add button "reiniciar pontos"

--- app/src/main/java/br/edu/ifsp/scl/sc3046656/trucoscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3046656/trucoscoreboard/MainActivity.kt
@@ -7,28 +7,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.edu.ifsp.scl.sc3046656.trucoscoreboard.ui.theme.TrucoScoreBoardTheme
 
@@ -66,10 +58,15 @@ fun TelaComDoisContadores(modifier: Modifier = Modifier) {
                 .weight(1f)
                 .fillMaxWidth()
         )
-        HorizontalDivider(
-            thickness = 2.dp,
-            color = Color.Gray
-        )
+
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {counterA = 0; counterB = 0}
+            ) {
+            Text("Reiniciar pontos")
+        }
+
         ContadorTimeB(
             counterA = counterA,
             counterB = counterB,
